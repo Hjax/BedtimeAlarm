28d840a5bccc7d8c81dbb0b66705ada55958c84f
Kevin Konrad <Kevintkonrad@gmail.com>
2015-12-05 13:19:03 -0500

    added some basic buttons

--- BTA.py
+++ BTA.py
@@ -1,14 +1,33 @@
-import wx
+import wx, pyttsx,time
 
 class Example(wx.Frame):
     
     def __init__(self, *args, **kwargs):
         super(Example, self).__init__(*args, **kwargs) 
-            
         self.InitUI()
+
+        self.engine = pyttsx.init()
+        self.engine.setProperty('rate', 130)
         
     def InitUI(self):    
 
+        panel = wx.Panel(self)
+
+        hbox = wx.BoxSizer()
+        sizer = wx.GridSizer(2, 2, 2, 2)
+
+        btn1 = wx.Button(panel, label='Nag')
+        btn2 = wx.Button(panel, label='2')
+        btn3 = wx.Button(panel, label='3')
+        btn4 = wx.Button(panel, label='4')
+
+        sizer.AddMany([btn1, btn2, btn3, btn4])
+
+        hbox.Add(sizer, 0, wx.ALL, 15)
+        panel.SetSizer(hbox)
+
+        btn1.Bind(wx.EVT_BUTTON, self.Nag)
+        
         menubar = wx.MenuBar()
         fileMenu = wx.Menu()
         fitem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
@@ -24,6 +43,10 @@ class Example(wx.Frame):
         
     def Quit(self, e):
         self.Close()
+
+    def Nag(self, e):
+        self.engine.say("It is time for you to go to bed")
+        self.engine.runAndWait()
 
 def main():
     
