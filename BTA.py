3496269ff62a068041a2733f2263e42cbc8d73e6
Kevin Konrad <Kevintkonrad@gmail.com>
2015-12-05 13:03:51 -0500

    Updated to a better starting point

--- BTA.py
+++ BTA.py
@@ -1,12 +1,36 @@
-#!/usr/bin/python
-
-# simple.py
-
 import wx
 
-app = wx.App()
+class Example(wx.Frame):
+    
+    def __init__(self, *args, **kwargs):
+        super(Example, self).__init__(*args, **kwargs) 
+            
+        self.InitUI()
+        
+    def InitUI(self):    
 
-frame = wx.Frame(None, -1, 'simple.py')
-frame.Show()
+        menubar = wx.MenuBar()
+        fileMenu = wx.Menu()
+        fitem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
+        menubar.Append(fileMenu, '&File')
+        self.SetMenuBar(menubar)
+        
+        self.Bind(wx.EVT_MENU, self.Quit, fitem)
 
-app.MainLoop()
+        self.SetSize((800, 600))
+        self.SetTitle('Bed Time Alarm Pre-Alpha')
+        self.Centre()
+        self.Show(True)
+        
+    def Quit(self, e):
+        self.Close()
+
+def main():
+    
+    ex = wx.App()
+    Example(None)
+    ex.MainLoop()    
+
+
+if __name__ == '__main__':
+    main()
